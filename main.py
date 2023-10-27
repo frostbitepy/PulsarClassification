fd5a101788677f52c43133161bb27c3344eefc2f
Francisco José Ruiz Vera <francisco@regionalseguros.com.py>
2023-10-27 10:15:40 -0300

    Updated main file

--- main.py
+++ main.py
@@ -1,38 +1,52 @@
-import sys
-from tensorflow import keras
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import argparse
 import pickle
-
-MAX_LENGTH = 100
-PADDING_TYPE = 'post'
-TRUNC_TYPE = 'post'
-TOKENIZER_PATH = 'tokenizer.pkl'
-MODEL_PATH = 'model.h5'
-
-# Load the tokenizer from the file
-tokenizer_filename = TOKENIZER_PATH
-with open(tokenizer_filename, 'rb') as f:
-    tokenizer = pickle.load(f)
-
-# Load Tensorflow model
-model = keras.models.load_model(MODEL_PATH)
-print(model.summary())
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 
-if len(sys.argv)>0:
+""" 
+Este código te permite cargar un modelo entrenado y un normalizador guardado desde archivos pickle y realizar 
+predicciones en nuevos datos de entrada especificados en un archivo CSV. Puedes ejecutar este script desde la 
+terminal proporcionando las rutas a los archivos del modelo, el normalizador y los datos de entrada. Por ejemplo:
+python main.py --modelo modelo_entrenado.pkl --normalizador normalizador.pkl --entrada datos_entrada.csv
+"""
+# Función para cargar el modelo y el normalizador
+def cargar_modelo_y_normalizador(modelo_path, normalizador_path):
+    with open(modelo_path, 'rb') as model_file:
+        modelo = pickle.load(model_file)
+    
+    with open(normalizador_path, 'rb') as scaler_file:
+        normalizador = pickle.load(scaler_file)
+    
+    return modelo, normalizador
 
-    arguments = sys.argv[1:]
+# Función para realizar predicciones
+def predecir(modelo, normalizador, datos_entrada):
+    # Aplica la transformación de normalización a los datos de entrada
+    datos_normalizados = normalizador.transform(datos_entrada)
+    
+    # Realiza predicciones
+    predicciones = modelo.predict(datos_normalizados)
+    
+    return predicciones
 
-    # Process the arguments
-    for arg in arguments:
-        print(arg)
+if __name__ == "__main__":
+    # Configurar argumentos desde la terminal
+    parser = argparse.ArgumentParser(description="Realizar predicciones con un modelo entrenado.")
+    parser.add_argument("--modelo", required=True, help="Ruta al archivo del modelo entrenado (pickle).")
+    parser.add_argument("--normalizador", required=True, help="Ruta al archivo del normalizador (pickle).")
+    parser.add_argument("--entrada", required=True, help="Ruta al archivo CSV de entrada para predicciones.")
+    args = parser.parse_args()
 
-    sequences = tokenizer.texts_to_sequences(arguments)
-    padded = pad_sequences(sequences, maxlen=MAX_LENGTH, padding=PADDING_TYPE, truncating=TRUNC_TYPE)
+    # Cargar el modelo y el normalizador
+    modelo, normalizador = cargar_modelo_y_normalizador(args.modelo, args.normalizador)
 
-    print(model.predict(padded))
+    # Cargar datos de entrada para predicciones desde un archivo CSV
+    datos_entrada = pd.read_csv(args.entrada)
 
+    # Realizar predicciones
+    predicciones = predecir(modelo, normalizador, datos_entrada)
 
-else:
-    print('0 or more than 1 arguments received')
+    # Imprimir las predicciones
+    print("Predicciones:")
+    print(predicciones)
